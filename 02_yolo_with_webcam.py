cc5501e3553a0e2def393342c7c0a64d3446a95b
TilenStern <tilen.sternn@gmail.com>
2025-03-01 15:42:55 +0100

    ⚡️ better data visualization for the webcam (02)

--- 02_yolo_with_webcam.py
+++ 02_yolo_with_webcam.py
@@ -1,31 +1,59 @@
 from ultralytics import YOLO 
-import cv2 as cv 
+import cv2
+import cvzone
+import math
 
 def main():
-    capture = cv.VideoCapture(0)
+    capture = cv2.VideoCapture(0)
     change_res(capture, 640, 480)
 
     model = YOLO('yolo_weights\\yolov8n.pt')
+
+    # class of all detectable objects
+    class_names = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
     while True: 
         isTrue, frame = capture.read()
         results = model(frame, stream=True)
 
-        for res in results: 
-            boxes = res.boxes
-            print(boxes)
+        # yolo model can read multiple images at once so we have to iterate over them 
+        # even if we only have one image
+        for im in results: 
+
+            # read the results of each image and store it into a list boxes
+            boxes = im.boxes
             for box in boxes: 
+
+                # this is still a (1,4) tensor (list in a list) so we have to unpack it even though 
+                # there is only a single object in this tensor 
                 x1, y1, x2, y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                cv.rectangle(frame, (x1, y1), (x2, y2), (0,0,255), 4)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0,0,255), 4)
 
-        cv.imshow('Live video', frame)
+                # reading a confidence of reading from over detected object
+                conf = math.ceil(box.conf[0]*100) / 100
+                print(conf)
 
-        if cv.waitKey(20) & 0xFF==ord('d'): 
+                # displaying a class name
+                object_id = int(box.cls[0])
+                cvzone.putTextRect(frame, f'{conf} {class_names[object_id]}', (x1, max(10, y1 - 10)), scale=1, thickness=1)
+        cv2.imshow('Live video', frame)
+
+        if cv2.waitKey(20) & 0xFF==ord('d'): 
             break 
 
     capture.release()
-    cv.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 
 def change_res(capture, width, height):
